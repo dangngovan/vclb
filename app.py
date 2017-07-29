ef40d388ba097fc4f6f418761112d3102ffacb4f
Ngo Van Dang <dangngovan@vccorp.vn>
2017-07-29 10:49:25 +0700

    update

--- app.py
+++ app.py
@@ -19,22 +19,23 @@ def index():
     sw85_pw = "aVi2nCZXBib4qDykaJ6w"
     sw1702_pw = "Iz7DFrey19QyXvJNVanP"
     oid_bw_quota = "1.3.6.1.2.1.2.2.1.5"
-    oid_bw_curent = "1.3.6.1.2.1.2.2.1.16"
-    # sw85 (Po5) -> Core
-    sw85_to_swcore_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5005"%(snmp_command,user,sw85_pw,oid_bw_quota)).split(": ")[1]
-    sw85_to_swcore_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5005"%(snmp_command,user,sw85_pw,oid_bw_curent)).split(": ")[1]
+    oid_bw_curent_out = "1.3.6.1.2.1.2.2.1.16"
+    oid_bw_curent_in = "1.3.6.1.2.1.2.2.1.10"
+    # sw85 (Po2) -> Core
+    sw85_to_swcore_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5002"%(snmp_command,user,sw85_pw,oid_bw_quota_out)).split(": ")[1]
+    sw85_to_swcore_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5002"%(snmp_command,user,sw85_pw,oid_bw_curent_out)).split(": ")[1]
 
     # sw1702 (Po1)-> swcore
-    sw1702_to_swcore_quota = commands.getoutput("%s %s -a MD5 -A %s 123.30.170.2 %s.5001"%(snmp_command,user,sw1702_pw,oid_bw_quota)).split(": ")[1]
-    sw1702_to_swcore_curent = commands.getoutput("%s %s -a MD5 -A %s 123.30.170.2 %s.5001"%(snmp_command,user,sw1702_pw,oid_bw_curent)).split(": ")[1]
+    sw1702_to_swcore_quota = commands.getoutput("%s %s -a MD5 -A %s 123.30.170.2 %s.5001"%(snmp_command,user,sw1702_pw,oid_bw_quota_out)).split(": ")[1]
+    sw1702_to_swcore_curent = commands.getoutput("%s %s -a MD5 -A %s 123.30.170.2 %s.5001"%(snmp_command,user,sw1702_pw,oid_bw_curent_out)).split(": ")[1]
 
     # sw84 (Po1) -> sw85
-    sw84_to_sw85_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.4 %s.5001"%(snmp_command,user,sw84_pw,oid_bw_quota)).split(": ")[1]
-    sw84_to_sw85_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.4 %s.5001"%(snmp_command,user,sw84_pw,oid_bw_curent)).split(": ")[1]
+    sw84_to_sw85_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.4 %s.5001"%(snmp_command,user,sw84_pw,oid_bw_quota_out)).split(": ")[1]
+    sw84_to_sw85_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.4 %s.5001"%(snmp_command,user,sw84_pw,oid_bw_curent_out)).split(": ")[1]
 
-    #sw85  (Po4) -> svr27126
-    sw85_to_svr27126_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5004"%(snmp_command,user,sw85_pw,oid_bw_quota)).split(": ")[1]
-    sw85_to_svr27126_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.5004"%(snmp_command,user,sw85_pw,oid_bw_curent)).split(": ")[1]
+    #sw85  (gi2/0/42-43-44-45) -> svr27126
+    sw85_to_svr27126_quota = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.10642"%(snmp_command,user,sw85_pw,oid_bw_quota_in)).split(": ")[1]
+    sw85_to_svr27126_curent = commands.getoutput("%s %s -a MD5 -A %s 123.31.8.5 %s.10642"%(snmp_command,user,sw85_pw,oid_bw_curent_in)).split(": ")[1]
 
     data = [{
             "Dich Vu":"CND",
@@ -56,7 +57,7 @@ def index():
             "Bang Thong (MB)":float(sw84_to_sw85_quota)/100000,
             "Bang Thong Hien Tai (MB)":float(sw84_to_sw85_curent)/100000,
             "Ty Le(%)": float(sw84_to_sw85_curent)/float(sw84_to_sw85_quota)* 100,
-            "May chu1":float(sw85_to_svr27126_curent)/10000
+            "May chu1":float(sw85_to_svr27126_curent)*4/10000
 
         }]
     # other column settings -> http://bootstrap-table.wenzhixin.net.cn/documentation/#column-options
